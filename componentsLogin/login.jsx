21b4ea3187aa6222af92fda49d8b343b4ed7a0f4
nickiowaspain <nickholtan@gmail.com>
2017-01-04 19:38:34 -0800

    adding untracked files

---
+++ componentsLogin/login.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {browserHistory } from 'react-router'
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          User: <input type="text"></input><br/>
+          Password: <input type="text"></input><br/>
+          <button type="button" onClick={function(){browserHistory.push('roleselector')}}>Login</button>
+          <button type="button" onClick={function(){browserHistory.push('signup'); console.log(browserHistory)}}>Signup</button><br/>
+          <button type="submit">Forgot Password?</button>
+          
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
---
+++ componentsLogin/signup.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Signup extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          User: <input type="text"></input><br/>
+          Password: <input type="text"></input><br/>
+          Email: <input type="text"></input><br/>
+          <button type="submit">Submit</button><br/>
+          
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Signup;
