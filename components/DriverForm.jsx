c4b5a5dcf62e8e1704ce178169cc88948c0704d5
nickiowaspain <nickholtan@gmail.com>
2017-01-05 16:16:22 -0800

    fixed rider page and now server is sending back info on both rider and driver

--- components/DriverForm.jsx
+++ components/DriverForm.jsx
@@ -1,39 +1,5 @@
 import React from 'react';
 
-// class DriverForm extends React.Component {
-
-//   render() {
-//     return (
-//       <div>
-//         <form>
-//           Name: <input type="text" ></input><br/>
-//           Destination: <input type="text" id="destination"></input><br/>
-//           Origin: <input type="text"></input><br/>
-//           Departure Date: <input type="text"></input><br/>
-//           # of Open Seats: <input type="text"></input><br/>
-//           Price: <input type="text"></input><br/>
-//            <button type="submit" 
-
-// // onClick={function(){$.ajax({
-// //   type: "POST",
-// //   url: 'http://localhost:3000/random'
-  
-// // }).then(function(data){
-// //   console.log(data)
-// // })
-
-// // }}
-
-// >Submit</button>
-
-//         </form>
-
-//       </div>
-//     );
-//   }
-// }
-
-//new component that we got from
 class DriverForm extends React.Component {
   constructor(props) {
     super(props);
@@ -59,15 +25,9 @@ class DriverForm extends React.Component {
     console.log(this.state.destination, this.state.origin)
     event.preventDefault();
 
+$.post( 'http://localhost:3000/random', {destination: this.state.destination, origin: this.state.origin} ).then(function(data){console.log(data)})
 
-        $.ajax({
-          type: "POST",
-          url: 'http://localhost:3000/random',
-          
-          
-        }).then(function(data){
-          console.log(data)
-        })
+
 
 
   }
