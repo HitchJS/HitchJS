632b2884500f9493ba386f66b1367002bdd2a7e7
nickiowaspain <nickholtan@gmail.com>
2017-01-06 10:29:53 -0800

    for real final commit with correct comments on jsx page

--- components/RoleSelector.jsx
+++ components/RoleSelector.jsx
@@ -5,10 +5,10 @@ class RoleSelector extends React.Component {
   render() {
     return (
       <div className="roleSelector">
-        // below on user clicking button we are using browserHistory to push /driver and change route to the driver or rider page
+        {/*below on user clicking button we are using browserHistory to push /driver and change route to the driver or rider page*/}
         <button className="driver-btn" type="button" onClick={function(){browserHistory.push('driver')}}>Driver</button>
         <button className="rider-btn" type="button" onClick={function(){browserHistory.push('rider')}}>Rider</button>
-       // below may not being used but may be used in the future
+       {/* below may not being used but may be used in the future*/}
        {this.props.children}
       </div>
     );
