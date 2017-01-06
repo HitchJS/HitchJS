eb61f2996185ff1e64928be39a77a76197721a41
Brandon Yuan <Brandon@Brandons-MacBook-Pro.local>
2017-01-05 17:24:14 -0800

    styling

--- components/RoleSelector.jsx
+++ components/RoleSelector.jsx
@@ -3,7 +3,7 @@ import {browserHistory } from 'react-router'
 class RoleSelector extends React.Component {
   render() {
     return (
-      <div>
+      <div className="roleSelector">
         <button type="button" onClick={function(){browserHistory.push('driver')}}>Driver</button>
         <button type="button" onClick={function(){browserHistory.push('rider')}}>Rider</button>
        
